29a46d63232511eb4e99a02a859260f2d730348e
unknown <hr.jeon28@gmail.com>
2025-02-02 11:06:46 +0900

    page routing - Dynamic Segments

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,8 @@
+import { useSearchParams } from "react-router-dom";
+
 const Home = () => {
+  const [params, serParms] = useSearchParams();
+  console.log(params.get("value"));
   return <div>Home</div>;
 };
 export default Home;
